cdd5efe9630814e8f4acca4b548d672523230eca
Karol Swieca <swieca.karol@gmail.com>
2019-01-03 14:15:04 +0100

    added missing workouts to state

--- src/store/state.js
+++ src/store/state.js
@@ -202,6 +202,158 @@ export default {
           isFinished: false
         }
       ]
+    },
+    {
+      id: 3,
+      name: 'tydzień A trening C',
+      excercises: [
+        {
+          name: 'Przysiad ze sztangą',
+          excerciseKey: 'squat',
+          lastSeriesAMRAP: true,
+          series: ['5', '5'],
+          seriesInfo: '2x5 | 1x5+',
+          isFinished: false
+        },
+        {
+          name: 'Wyciskanie leżąc',
+          excerciseKey: 'bench',
+          lastSeriesAMRAP: true,
+          series: ['5', '5'],
+          seriesInfo: '2x5 | 1x5+',
+          isFinished: false
+        },
+        {
+          name: 'Podciąganie',
+          excerciseKey: 'pullUp',
+          lastSeriesAMRAP: false,
+          series: ['8', '8', '8'],
+          seriesInfo: '3x6-8',
+          isFinished: false
+        },
+        {
+          name: 'Facepulls',
+          excerciseKey: 'facepull',
+          lastSeriesAMRAP: false,
+          series: ['8', '8', '8'],
+          seriesInfo: '3x6-8',
+          isFinished: false
+        }
+      ]
+    },
+    {
+      id: 4,
+      name: 'tydzień B trening A',
+      excercises: [
+        {
+          name: 'Przysiad ze sztangą',
+          excerciseKey: 'squat',
+          lastSeriesAMRAP: true,
+          series: ['5', '5'],
+          seriesInfo: '2x5 | 1x5+',
+          isFinished: false
+        },
+        {
+          name: 'Wyciskanie nad głowę sztangi stojąc',
+          excerciseKey: 'ohp',
+          lastSeriesAMRAP: true,
+          series: ['5', '5'],
+          seriesInfo: '2x5 | 1x5+',
+          isFinished: false
+        },
+        {
+          name: 'Wiosłowanie sztangą',
+          excerciseKey: 'rows',
+          lastSeriesAMRAP: false,
+          series: ['8', '8', '8'],
+          seriesInfo: '3x6-8',
+          isFinished: false
+        },
+        {
+          name: 'Uginanie sztangi na biceps',
+          excerciseKey: 'bicep',
+          lastSeriesAMRAP: false,
+          series: ['12', '12', '12'],
+          seriesInfo: '3x10-12',
+          isFinished: false
+        }
+      ]
+    },
+    {
+      id: 5,
+      name: 'tydzień B trening B',
+      excercises: [
+        {
+          name: 'Martwy ciag',
+          excerciseKey: 'deadlift',
+          lastSeriesAMRAP: true,
+          series: ['5', '5'],
+          seriesInfo: '2x5 | 1x5+',
+          isFinished: false
+        },
+        {
+          name: 'Wyciskanie leżąc',
+          excerciseKey: 'bench',
+          lastSeriesAMRAP: true,
+          series: ['5', '5'],
+          seriesInfo: '2x5 | 1x5+',
+          isFinished: false
+        },
+        {
+          name: 'Podciąganie',
+          excerciseKey: 'pullUp',
+          lastSeriesAMRAP: false,
+          series: ['8', '8', '8'],
+          seriesInfo: '3x6-8',
+          isFinished: false
+        },
+        {
+          name: 'Dipsy',
+          excerciseKey: 'tricep',
+          lastSeriesAMRAP: false,
+          series: ['8', '8', '8'],
+          seriesInfo: '3x6-8',
+          isFinished: false
+        }
+      ]
+    },
+    {
+      id: 6,
+      name: 'tydzień B trening C',
+      excercises: [
+        {
+          name: 'Przysiad ze sztangą',
+          excerciseKey: 'squat',
+          lastSeriesAMRAP: true,
+          series: ['5', '5'],
+          seriesInfo: '2x5 | 1x5+',
+          isFinished: false
+        },
+        {
+          name: 'Wyciskanie nad głowę sztangi stojąc',
+          excerciseKey: 'ohp',
+          lastSeriesAMRAP: true,
+          series: ['5', '5'],
+          seriesInfo: '2x5 | 1x5+',
+          isFinished: false
+        },
+        {
+          name: 'Wiosłowanie sztangą',
+          excerciseKey: 'rows',
+          lastSeriesAMRAP: false,
+          series: ['8', '8', '8'],
+          seriesInfo: '3x6-8',
+          isFinished: false
+        },
+        {
+          name: 'Facepulls',
+          excerciseKey: 'facepull',
+          lastSeriesAMRAP: false,
+          series: ['8', '8', '8'],
+          seriesInfo: '3x6-8',
+          isFinished: false
+        }
+      ]
     }
   ]
 }
